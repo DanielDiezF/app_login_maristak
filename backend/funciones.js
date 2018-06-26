7c3b89dcf14b03f2a99199233668279c147f2888
Daniel Diez <danieldiezfuente@gmail.com>
2018-06-26 09:59:11 +0200

    Cambiada colección lusers a users por respeto

--- backend/funciones.js
+++ backend/funciones.js
@@ -2,13 +2,13 @@ var mongo = require('mongodb');
 var MongoClient = require('mongodb').MongoClient;
 var dburl = "mongodb://localhost:27017/";
 var dbo;
-var lusers;
+var users;
 var sesiones;
 
 MongoClient.connect(dburl)
 .then(function(db) {
   dbo = db.db("loginapp");
-  lusers = dbo.collection("lusers");
+  users = dbo.collection("users");
   sesiones = dbo.collection("sesiones");
   deleteExpiredSessions();
 })
@@ -19,17 +19,17 @@ MongoClient.connect(dburl)
 function nuevoUsuario(usuario){
   return comprobarUsuario(usuario.nombre, usuario.email)
   .then(function(){
-    return lusers.insertOne(usuario);
+    return users.insertOne(usuario);
   })
 }
 
 function comprobarUsuario(nombre, email){
-  return lusers.findOne({'nombre': nombre})
+  return users.findOne({'nombre': nombre})
 	.then(function(res) {
 		if(res){
       return Promise.reject('Nombre en uso');
     }
-    return lusers.findOne({'email': email})
+    return users.findOne({'email': email})
   })
   .then(function(res) {
 		if(res){
@@ -39,7 +39,7 @@ function comprobarUsuario(nombre, email){
 }
 
 function login(datosLogin){
-  return lusers.findOne({'nombre': datosLogin[0], 'pass': datosLogin[1]})
+  return users.findOne({'nombre': datosLogin[0], 'pass': datosLogin[1]})
   .then(function(res){
     if(res){
       return crearSesion(res._id);
@@ -63,7 +63,7 @@ function getUserNameBySessId(sessID){
   let searchID = new mongo.ObjectID(sessID);
   return sesiones.findOne({_id: searchID})
   .then(function(res){
-    return lusers.findOne({_id: res.idUser});
+    return users.findOne({_id: res.idUser});
   })
   .then(function(res){
     return res.nombre;
